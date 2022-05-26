4e734efea42959876cb3459a190573eadf3add86
coderbennett <jbennett@csumb.edu>
2022-05-25 17:31:18 -0700

    add plugins to webpack config file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,32 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: 'index.html',
+        title: 'Jate Text Editor'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Jate Text Editor',
+        short_name: 'Jate',
+        description: 'A simple text editor.',
+        background_color: '#C72E0D',
+        theme_color: '#C72E0D',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons')
+          }
+        ]
+      })
     ],
 
     module: {
